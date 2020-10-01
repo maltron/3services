564594dcebaee30c94af9647c1d245ce338e74c9
Mauricio "Maltron" Leal <maltron@gmail.com>
2020-10-01 14:40:00 -0300

    Automated creation of Istio Objects

--- runWorkload.sh
+++ runWorkload.sh
@@ -1,1 +1,1 @@
-while true; do curl $(oc get route/microservice-a --output jsonpath='{.spec.port.targetPort}://{.spec.host}'); echo; sleep 1.5s; done
+while true; do curl $(oc get gateway/microservice --output=jsonpath='http://{.spec.servers[0].hosts[0]}'); echo; sleep 1.5s; done
